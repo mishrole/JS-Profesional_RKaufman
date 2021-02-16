5e0f3c4a12ae6ea69e6fc47d932555770211d4e2
Mitchell Rodríguez <mitrodle@gmail.com>
2021-02-15 23:24:19 -0500

    Fixed: El primer plugin

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,7 +1,15 @@
 // "Clase" MediaPlayer recibe un objeto
 function MediaPlayer(config) {
     this.media = config.el; // { el: video }
+    this.plugins = config.plugins || [];
+    this._initPlugins();
 }
+
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+};
     
 MediaPlayer.prototype.play = function() {
     this.media.play();
@@ -14,5 +22,17 @@ MediaPlayer.prototype.pause = function() {
 MediaPlayer.prototype.togglePlay = function() {
     this.media.paused ? this.play() : this.pause()
 };
+
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+}
+
+MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
+}
+
+MediaPlayer.prototype.toggleMute = function() {
+    this.media.muted ? this.unmute() : this.mute()
+}
 
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
 // Instancia de MediaPlayer
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
 
-const button = document.querySelector('button');
-button.onclick = () => player.togglePlay();
+const play = document.querySelectorAll('button')[0];
+const mute = document.querySelectorAll('button')[1];
+play.onclick = () => player.togglePlay();
+mute.onclick = () => player.toggleMute();
