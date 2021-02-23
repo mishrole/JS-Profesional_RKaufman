a978ab783a90d50545a82207db96fafdb8cd4be6
Mitchell Rodríguez <mitrodle@gmail.com>
2021-02-23 13:41:04 -0500

    Add: ServiceWorker

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,21 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 // Instancia de MediaPlayer
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+const player = new MediaPlayer({
+    el: video,
+    plugins: [new AutoPlay(), new AutoPause] });
 
-const play = document.querySelectorAll('button')[0];
-const mute = document.querySelectorAll('button')[1];
+const play = document.querySelector('#btnPlay');
+const mute = document.querySelector('#btnMute');
 play.onclick = () => player.togglePlay();
 mute.onclick = () => player.toggleMute();
+
+// Detectar si el navegador del usuario le da soporte a Service Workers
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/serviceWorker.js').catch(error => {
+        console.error(error.message);
+    });
+}
---
+++ assets/plugins/AutoPause.js
@@ -0,0 +1,59 @@
+class AutoPause {
+    // Como vamos a usar el valor de threshold en varios lugares, lo definimos en un constructor
+    constructor() {
+        this.threshold = 0.25
+        // Definimos quién es this permanentemente para referirnos a la instancia del plugin
+        this.handleIntersection = this.handleIntersection.bind(this);
+        this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
+        
+        // Manejamos el estado si es pausado por cambio de pestaña
+        this.pausedByVisibility = false;
+    }
+
+    run(player) {
+        // Vamos a necesitar usar los métodos del player, por eso los guardamos en una instancia de la clase
+        this.player = player;
+
+        const observer = new IntersectionObserver(
+            this.handleIntersection, {
+                threshold: this.threshold
+            }
+        )
+        
+        observer.observe(this.player.media)
+
+        document.addEventListener("visibilitychange", this.handleVisibilityChange)
+    }
+
+    // Pausa si se sale de la vista, y reanuda si regresa
+    handleIntersection(entries) {
+        const entry = entries[0];
+        const isVisible = entry.intersectionRatio >= this.threshold;
+        // Como tenemos la instancia de la clase, podemos usar play y pause
+        if(isVisible) {
+            this.player.play();
+        } else {
+            this.player.pause();
+        }
+    }
+
+    // Pausa si cambia de pestaña, y reanuda si regresa
+    handleVisibilityChange() {
+        const isVisible = document.visibilityState === "visible";
+        
+        if(isVisible) {
+            if(this.pausedByVisibility) {
+                this.player.play();
+            }
+        } else {
+            if(!this.player.media.paused) {
+                this.player.pause();
+                this.pausedByVisibility = true;
+            } else {
+                this.pausedByVisibility = false;
+            }
+        }
+    }
+}
+
+export default AutoPause;
